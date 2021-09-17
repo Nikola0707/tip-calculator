618c980f0262145b2175282b29eff7254a26a8bf
Nikola0707 <ristoski.nikola@gmail.com>
2021-09-17 16:43:57 +0200

    Splitter title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Calculator from './components/Calculator.component';
 function App() {
   return (
    <div className="app-container">
+     <div className="title">
+       <span>SPLI</span>
+       <span>TTER</span>
+     </div>
       <Calculator />
    </div>
   );
