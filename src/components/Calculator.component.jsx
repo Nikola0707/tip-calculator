5ae58a38a86e6b996c1bd96eca5444285df7324c
Nikola0707 <ristoski.nikola@gmail.com>
2021-09-17 14:59:20 +0200

    Finishing with desktop styling

--- src/components/Calculator.component.jsx
+++ src/components/Calculator.component.jsx
@@ -1,49 +1,59 @@
 import "./Calculator.style.css";
+import { FaUserAlt } from "react-icons/fa";
 
 const Calculator = () => {
   return (
     <div className="calculator-container">
       <div className="left-container">
-        <div className="bill-input-container">
-          <label htmlFor="bill">Bill</label>
-          <input type="text" placeholder="$ 0" />
+        <label htmlFor="bill">Bill</label>
+        <div className="bill-input-container" id="f">
+          <div>$</div>
+          <input type="text" placeholder="0" />
         </div>
         <div className="select-tip-container">
-          <p className="select-tip-paragraph">Select Tip %</p>
+          <label htmlFor="tip">Select Tip %</label>
           <div className="select-tip-btns">
-            <div className="tip-box">5%</div>
-            <div className="tip-box">10%</div>
-            <div className="tip-box">15%</div>
-            <div className="tip-box">25%</div>
-            <div className="tip-box">50%</div>
-            <div className="tip-box tip-box-custom">Custom</div>
+            <div className="tip-box tip-box-1" tabindex="0">5%</div>
+            <div className="tip-box tip-box-2" tabIndex="0">10%</div>
+            <div className="tip-box tip-box-3" tabIndex="0">15%</div>
+            <div className="tip-box tip-box-4" tabIndex="0">25%</div>
+            <div className="tip-box tip-box-5" tabIndex="0">50%</div>
+            <input type="text" name="custom-tip" placeholder="Custom"/>
           </div>
         </div>
         <div className="number-of-ppl-container">
           <label htmlFor="ppl">Number of People</label>
-          <input type="text" placeholder="icon  0" />
+          <div className="people-number-input-container">
+            <div><FaUserAlt /></div>
+            <input type="text" placeholder="0" />
+          </div>
         </div>
       </div>
       <div className="right-container">
-        <div className="tip-amount">
-          <div>
-            <p className="price-explanation">
-              Tip Amount <span className="divide">/ person</span>
-            </p>
+        <div className="tip-and-total-price-container">
+          <div className="tip-amount">
+            <div>
+              <p className="price-explanation">
+                Tip Amount <span className="divide">/ person</span>
+              </p>
+            </div>
+            <div>
+              <p className="price">$0.00</p>
+            </div>
           </div>
-          <div>
-            <p className="price">$0.00</p>
+          <div className="total">
+            <div>
+              <p className="price-explanation">
+                Total <span className="divide">/ person</span>
+              </p>
+            </div>
+            <div>
+              <p className="price">$0.00</p>
+            </div>
           </div>
         </div>
-        <div className="total">
-          <div>
-            <p className="price-explanation">
-              Total <span className="divide">/ person</span>
-            </p>
-          </div>
-          <div>
-            <p className="price">$0.00</p>
-          </div>
+        <div className="reset-btn">
+          <button>RESET</button>
         </div>
       </div>
     </div>
